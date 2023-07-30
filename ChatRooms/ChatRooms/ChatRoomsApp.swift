db22134796dde0da2750849788c8b76638abca71
Zdenko Čepan <zdenkocepan@MacStudlaZdenko.docsis.vodafone.cz>
2023-07-30 14:34:29 +0200

    create project

---
+++ ChatRooms/ChatRooms/ChatRoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRoomsApp.swift
+//  ChatRooms
+//
+//  Created by Zdenko Čepan on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatRoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
